bece0519279f02fc9e8b278283f8a7ea1a44f348
JackNiu <540051856@qq.com>
2017-07-01 17:31:52 +0800

    构建工程

---
+++ src/main/java/com/jackniu/SpringProjectDataApplication.java
@@ -0,0 +1,12 @@
+package com.jackniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectDataApplication.class, args);
+	}
+}
